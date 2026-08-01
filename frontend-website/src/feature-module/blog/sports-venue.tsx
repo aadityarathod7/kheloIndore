584022cc460b62be8655005cec25454a52658117
Aaditya <aadityarathod7@gmail.com>
2026-08-01 16:46:10 +0530

    Fix missing Link import in sports-venue.tsx

--- frontend-website/src/feature-module/blog/sports-venue.tsx
+++ frontend-website/src/feature-module/blog/sports-venue.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "../../ApiUrl";
 import Loader from "../loader/loader";
